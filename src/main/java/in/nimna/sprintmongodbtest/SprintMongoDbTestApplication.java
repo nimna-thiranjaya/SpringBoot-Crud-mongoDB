a5f78c31caefd46973afb159ebb5362eb91a372d
Nimna Thiranjaya <nimnathiranjaya523@gmail.com>
2023-02-09 14:09:38 +0530

    folder structure

---
+++ src/main/java/in/nimna/sprintmongodbtest/SprintMongoDbTestApplication.java
@@ -0,0 +1,13 @@
+package in.nimna.sprintmongodbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintMongoDbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintMongoDbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/nimna/sprintmongodbtest/SprintMongoDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nimna.sprintmongodbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintMongoDbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
